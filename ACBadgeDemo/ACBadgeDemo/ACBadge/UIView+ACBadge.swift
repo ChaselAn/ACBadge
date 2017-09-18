b6a5b6889fe1157871e7f919c37ffcb5469dac4c
ancheng <865770853@qq.com>
2017-09-18 11:39:57 +0800

    -> swift4

--- ACBadgeDemo/ACBadgeDemo/ACBadge/UIView+ACBadge.swift
+++ ACBadgeDemo/ACBadgeDemo/ACBadge/UIView+ACBadge.swift
@@ -165,7 +165,7 @@ extension UIView {
       ac_badge?.text = "\(num)"
     }
     let att = NSMutableAttributedString(string: ac_badge?.text ?? "")
-    att.addAttributes([NSFontAttributeName: ac_badge!.font], range: NSRange(location: 0, length: (ac_badge?.text ?? "").characters.count))
+    att.addAttributes([NSAttributedStringKey.font: ac_badge!.font], range: NSRange(location: 0, length: (ac_badge?.text ?? "").characters.count))
     ac_badge?.frame = att.boundingRect(with: CGSize.zero, options: [.usesLineFragmentOrigin,.usesFontLeading], context: nil)
     ac_badge?.frame.size.width = CGFloat(Int(ac_badge!.frame.size.width)) + 4
     ac_badge?.frame.size.height = CGFloat(Int(ac_badge!.frame.size.height)) + 4
